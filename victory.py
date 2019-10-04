6f1d4a24c50234809518fd99d133b02340f86640
LaenorSama <laenorsama@yandex.ru>
2019-10-04 23:29:05 +0500

    update

--- victory.py
+++ victory.py
@@ -22,6 +22,8 @@ wish_play = 'да'
 while wish_play != 'нет':
     # заведем счетчик правильных ответов игрока
     user_score = 0
+    # так же счетчик неправильных ответов
+    user_fail = 0
     # задаем вопросы
     for i in range(len(questions)):
         print('Вопрос', i + 1)
@@ -30,8 +32,13 @@ while wish_play != 'нет':
             user_score += 1
             print('Верно! :D')
         else:
+            user_fail += 1
             print('Неверно! :(')
-    # выводим % правильных ответов игрока
-    print('Вы набрали', user_score / 5 * 100, '%')
+    # выводим результаты
+    print('Верных ответов:', user_score)
+    print('Неверных ответов:', user_fail)
+    # выводим в %
+    print('% верных ответов', user_score / len(questions) * 100)
+    print('% неверных ответов', user_fail / len(questions) * 100)
     wish_play = input('Начнем сначала?')
     wish_play = wish_play.lower()
